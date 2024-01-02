7e63ac4d9d9118aa3bf1e023bb5dc5aa07b71fb7
sokhna-faye <155458319+sokhna-faye@users.noreply.github.com>
2024-01-03 07:50:53 +1400

    Add files via upload

---
+++ accueil.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+
+</div>
+<div class="menue">
+    <h4 class="ha">Bienvenue sur notre site web!</h4>
+    <ul class="ul1">
+    <li><a href="accueil">ACCUEIL</a></li><br><br>
+    <li><a href="register">Inscription </a></li><br><br>
+    <li><a href="login">Connexion</a></li><br><br>
+
+    </ul>
+
+
+    <div class="diva">
+        <img  class="img1"  src="images/i5.jpg" alt="">
+    </div>
+
+
+    <div class="divc">
+        <h1 class="hb">Bienvenue sur notre page d'accueil!</h1></div>
+        <p class="p1">
+            Si vous avez un compte, vous pouvez se connecter <br>
+            sinon s'inscrire
+            <ol>
+                <li>BECAYE KALELA</li>
+                <li>SOKHNA FAYE</li>
+                <li>COUMBA SOW</li>
+                <li>AMY THIAO</li>
+            </ol>
+        </p>
+</div>
+
+
+</div>
+<div class="div3">
+
+
+
+<ul class="ul2">
+    <li><a href="accueil">ACCUEIL</a></li><br><br>
+    <li><a href="register">Inscription </a></li><br><br>
+    <li><a href="login">Connexion</a></li><br><br>
+
+</ul>
+</div>
+<div class="ii">
+    <img class="logo" src="images/ii.jpg" alt="">
+</div>
+</body>
+
+</html>
---
+++ login.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="menue">
+        <ul class="ul1">
+        <li><a href="accueil">ACCUEIL</a></li><br><br>
+        <li><a href="register">Inscription </a></li><br><br>
+        <li><a href="login">Connexion</a></li><br><br>
+        </ul>
+        <div class="diva">
+            <img  class="img11"  src="images/i5.jpg" alt="">
+        </div>
+        <div class="ii">
+            <img class="logo" src="images/ii.jpg" alt="">
+        </div>
+    </div>
+   <marquee behavior="" direction=""> <h1 class="q1">CONNECTEZ-VOUS EN REMPLISSANT CES DEUX CHAMPS</h1></marquee>
+    <form id="form_login">
+        <h1 class="hi">CONNEXION</h1>
+
+        <label for="name">EMAIL***</label>
+        <input type="email" id="email" name="email" placeholder="veiller saisir votre email" required><br><br>
+        <label for="nom">M de passe</label>
+        <input type="password" id="mot_de_passe" name="mot_de_passe" placeholder="veiller saisir votre M de passe"
+        required><br><br>
+
+         <input class="aa"  type="submit" value="se connecter">
+<ul class="ty">
+         <li><a class="a2" href="register">s'inscrire </a></li><br><br>
+        </ul>
+
+</form>
+
+</body>
+</html>
